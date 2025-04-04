c0a0e4f4da6b913a2b9f925953c51e29680ce604
odd-git <oddgit@tuta.com>
2025-04-04 09:51:40 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.8fe6d62b2e9e4181af0b216fc24de5ea',
-  appName: 'weektodo-mobile-view',
-  webDir: 'dist',
+  "appId": "com.lovable.app",
+  "appName": "YourAppName",
+  "webDir": "www",
   server: {
     url: 'https://8fe6d62b-2e9e-4181-af0b-216fc24de5ea.lovableproject.com?forceHideBadge=true',
     cleartext: true
