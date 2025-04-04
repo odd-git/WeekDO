aa4270b4061438bdc92213bed46091d297b9aee1
odd-git <oddgit@tuta.com>
2025-04-04 09:58:14 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   "appId": "com.lovable.app",
   "appName": "YourAppName",
-  "webDir": "www",
+  "webDir": "dist",
   server: {
     url: 'https://8fe6d62b-2e9e-4181-af0b-216fc24de5ea.lovableproject.com?forceHideBadge=true',
     cleartext: true
